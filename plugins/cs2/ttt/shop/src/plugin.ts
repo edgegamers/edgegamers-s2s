b1b0983997ed6f14c094aeb6c56aaa9432ef02b0
ShookEagle <shookeagle@gmail.com>
2026-08-26 22:34:21 -0500

    fix: defer ttt shop api publication

--- plugins/cs2/ttt/shop/src/plugin.ts
+++ plugins/cs2/ttt/shop/src/plugin.ts
@@ -1,11 +1,9 @@
 import { plugin } from "@s2script/sdk/plugin";
 import type { TttCoreApi } from "@edgegamers/ttt-core";
 import type { TttKarmaApi } from "@edgegamers/ttt-karma";
-import type { TttShopApi } from "../api.d.ts";
 
 export default plugin((ctx) => {
   ctx.use<TttCoreApi>("@edgegamers/ttt-core");
   ctx.tryUse<TttKarmaApi>("@edgegamers/ttt-karma");
-  ctx.publish<TttShopApi>("@edgegamers/ttt-shop", {} as TttShopApi);
   console.log("[ttt-shop] loaded");
 });
